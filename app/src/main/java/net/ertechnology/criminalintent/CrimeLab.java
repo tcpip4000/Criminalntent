0477297ad181bc3b28396deb5c201af77f690398
Juan P. Daza P <juan.daza@er-technology.net>
2014-07-21 18:03:59 -0500

    Adding CrimeLab object

---
+++ app/src/main/java/net/ertechnology/criminalintent/CrimeLab.java
@@ -0,0 +1,49 @@
+package net.ertechnology.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by Juan on 21/07/2014.
+ */
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private Context mAppContext;
+    private ArrayList<Crime> mCrimes;
+
+    private CrimeLab(Context appContext) {
+        mAppContext = appContext;
+        mCrimes = new ArrayList<Crime>();
+        for (int i=0; i<100; i++) {
+            Crime c = new Crime();
+            c.setTitle("Crime #" + i);
+            c.setSolved(i%2 == 0);
+            mCrimes.add(c);
+        }
+    }
+
+
+    public static CrimeLab get(Context c) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+        }
+        return sCrimeLab;
+    }
+
+    public ArrayList<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+        for (Crime c : mCrimes) {
+            if (c.getId().equals(id)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+
+}
