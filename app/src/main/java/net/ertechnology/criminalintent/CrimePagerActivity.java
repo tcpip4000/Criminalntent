b0d5dec682b1cc98e514f3ae47a38789e131a189
Juan P. Daza P <juan.daza@er-technology.net>
2014-08-05 17:28:40 -0500

    Added pager

---
+++ app/src/main/java/net/ertechnology/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,42 @@
+package net.ertechnology.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+
+/**
+ * Created by fulano on 8/5/14.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+
+    private ViewPager mViewPager;
+    private ArrayList<Crime> mCrimes;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int i) {
+                Crime crime = mCrimes.get(i);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+    }
+}
